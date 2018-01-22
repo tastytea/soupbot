6d6d2842d354a8b0d80234d26ecdfa8898117724
tastytea <tastytea@tastytea.de>
2018-01-22 04:10:21 +0100

    tootgeneration works ok

--- src/soupbot.cpp
+++ src/soupbot.cpp
@@ -24,6 +24,8 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/filesystem.hpp>
+#include <random>
+#include <chrono>
 #include "version.hpp"
 
 namespace pt = boost::property_tree;
@@ -71,6 +73,14 @@ void read_config(pt::ptree &config, string &instance, string &access_token)
     }
 }
 
+void populate_vector(const pt::ptree &ingredients, const string &node, std::vector<string> &vector)
+{
+    for (const pt::ptree::value_type &value : ingredients.get_child(node))
+    {
+        vector.push_back(value.second.data());
+    }
+}
+
 int main(int argc, char *argv[])
 {
     pt::ptree config;
@@ -81,6 +91,69 @@ int main(int argc, char *argv[])
 
     pt::ptree ingredients;
     pt::read_json(filepath + "ingredients.json", ingredients);
-    
+    std::vector<string> vegetables;
+    std::vector<string> spices;
+    std::vector<string> fruits;
+    std::vector<string> misc;
+    std::vector<string> highprio;
+
+    populate_vector(ingredients, "vegetables", vegetables);
+    populate_vector(ingredients, "spices", spices);
+    populate_vector(ingredients, "fruits", fruits);
+    populate_vector(ingredients, "misc", misc);
+    populate_vector(ingredients, "highprio", highprio);
+
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
+    std::uniform_int_distribution<std::uint8_t> distribution(0, 3);
+    std::uniform_int_distribution<std::uint16_t> distribution_vegetables(0, vegetables.size() - 1);
+    std::uniform_int_distribution<std::uint16_t> distribution_spices(0, spices.size() - 1);
+    std::uniform_int_distribution<std::uint16_t> distribution_fruits(0, fruits.size() - 1);
+    std::uniform_int_distribution<std::uint16_t> distribution_misc(0, misc.size() - 1);
+    std::uniform_int_distribution<std::uint16_t> distribution_highprio(0, highprio.size() - 1);
+
+    auto dice = std::bind(distribution, generator);
+    auto vegdice = std::bind(distribution_vegetables, generator);
+    auto spicedice = std::bind(distribution_spices, generator);
+    auto fruitdice = std::bind(distribution_fruits, generator);
+    auto miscdice = std::bind(distribution_misc, generator);
+    auto priodice = std::bind(distribution_highprio, generator);
+
+    string toot = "Todays #soup is made of:\n\n";
+
+    toot += "1 part " + vegetables[vegdice()];
+    if (dice() > 0)
+        toot += ",\n1 part " + vegetables[vegdice()];
+    if (dice() > 1)
+        toot += ",\n1 part " + vegetables[vegdice()];
+
+    if (dice() > 1)
+        toot += ",\na few " + fruits[fruitdice()];
+
+    if (dice() > 1)
+        toot += ",\na good handful " + misc[miscdice()];
+    if (dice() > 1)
+        toot += ",\na good handful " + misc[miscdice()];
+
+    toot += ",\n" + spices[spicedice()];
+    if (dice() > 0)
+        toot += ",\n" + spices[spicedice()];
+    if (dice() > 1)
+        toot += ",\n" + spices[spicedice()];
+
+    toot += ",\nan ample amount of " + highprio[priodice()];
+    if (dice() > 1)
+        toot += " and " + highprio[priodice()];
+
+    toot += "\nand plenty oil.\n\n#bot";
+
+    cout << toot << '\n';
+
+
+    // string answer;
+    // std::uint16_t ret;
+    // Mastodon::API masto(instance, access_token);
+    // masto.set_useragent("soupbot/" + (string)global::version);
+
     return 0;
 }
